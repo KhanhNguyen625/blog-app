307c2a186a5772831056545a7bf20e45cb80bec2
Khanh Nguyen <kh4nhcodes@gmail.com>
2023-10-06 13:39:13 -0700

    First Commit
    - Home page created
    - Bootstrap article added
    - MVC article added
    -

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("home").addEventListener("click", function() {
+    // Replace 'your-link-here.html' with the actual URL of the page you want to link to.
+    window.location.href = "index.html";
+});
+
+document.getElementById("blog1").addEventListener("click", function() {
+    // Replace 'your-link-here.html' with the actual URL of the page you want to link to.
+    window.location.href = "bootstrap.html";
+});
+
+document.getElementById("btn-test").addEventListener("click", function() {
+    // Replace 'your-link-here.html' with the actual URL of the page you want to link to.
+    window.location.href = "index.html";
+});
